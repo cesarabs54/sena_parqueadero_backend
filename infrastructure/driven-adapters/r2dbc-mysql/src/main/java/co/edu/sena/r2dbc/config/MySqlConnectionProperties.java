801c3206843c18329c319e25354eaa8dfbf33ec4
Jorge Mario Mina Diaz <jmina@bancolombia.com.co>
2026-02-16 16:27:56 -0500

    Replace R2DBC PostgreSQL adapter with R2DBC MySQL adapter
    - Removed R2DBC PostgreSQL adapter and its associated configuration, helper classes, and tests.
    - Introduced R2DBC MySQL adapter with connection pooling using R2DBC.
    - Updated `application.yaml` to include MySQL-specific properties.
    - Adjusted dependencies and build configurations to reflect the switch from PostgreSQL to MySQL.

---
+++ infrastructure/driven-adapters/r2dbc-mysql/src/main/java/co/edu/sena/r2dbc/config/MySqlConnectionProperties.java
@@ -0,0 +1,12 @@
+package co.edu.sena.r2dbc.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "adapters.r2dbc")
+public record MySqlConnectionProperties(
+    String host,
+    Integer port,
+    String database,
+    String username,
+    String password) {
+}
