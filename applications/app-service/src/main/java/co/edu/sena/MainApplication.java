85fc90e4cdf2c325b6c3991ca4ea9853364473e3
Jorge Mario Mina Diaz <jmina@bancolombia.com.co>
2026-02-16 15:27:48 -0500

    Add initial setup for Clean Architecture project with Spring Boot integration
    - Configured Dockerfile for containerization with OpenJDK 25.
    - Included Gradle build files with dependency management and plugins for testing, code quality, and build caching.
    - Implemented a modular project structure following Clean Architecture principles.
    - Added entry point, domain, use-cases, and infrastructure layers.
    - Configured logging with log4j2 and set up application properties.
    - Created README with project overview and architecture details.
    - Ignored unnecessary files using .gitignore for various environments.

---
+++ applications/app-service/src/main/java/co/edu/sena/MainApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ applications/app-service/src/main/java/co/edu/sena/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.edu.sena.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.edu.sena.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
